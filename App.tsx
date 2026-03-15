d79488c42858a57811776d192e0ddf3b3065c50a
Joyce <joycemasallajorge2006@gmail.com>
2026-03-15 16:19:41 -0300

    adicionar o style={{ flex: 1 }} no SafeAreaView do App.tsx para rodar no celular

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Interface } from './src/view/Interface';
 
-
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
         <Interface />
       </SafeAreaView>
     </SafeAreaProvider>
