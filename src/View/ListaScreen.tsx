586390636221ef1ab3b8202f4787aa0d2f40950b
JOYCE MASALLA JORGE <1-24-11524@aluno.faminas.edu.br>
2026-03-24 22:20:44 -0300

    Adição do useFocusEffect para recarregar a lista das entidades sempre que entrar na tela da lista

--- src/View/ListaScreen.tsx
+++ src/View/ListaScreen.tsx
@@ -1,18 +1,39 @@
-import React from "react";
-import { View, Button, TextInput, Text, FlatList, TouchableOpacity, Modal } from "react-native";
-import { ControladoraViewModel } from "../viewModel/Controladora";
-import styles from "../styles/Styles";
+import React from 'react';
+import {
+    View,
+    Button,
+    TextInput,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    Modal,
+} from 'react-native';
+import { ControladoraViewModel } from '../viewModel/Controladora';
+import styles from '../styles/Styles';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect } from "@react-navigation/native";
+import { useState } from "react";
+import { useCallback } from "react";
+import { RegrasDaEntidade } from '../business/RegrasNegocio';
+import { Entidade } from '../model/Entidade';
 
 export function ListaScreen() {
     const viewModel = ControladoraViewModel();
     const navigation = useNavigation();
-    return (
 
+    const [entidades, setEntidades] = useState<Entidade[]>([]);
+
+    useFocusEffect(
+        useCallback(() => {
+            setEntidades([...RegrasDaEntidade.listaEntidadesEstatica]);
+        }, [])
+    );
+
+    return (
         <View style={styles.container}>
             <FlatList
-                data={viewModel.listaEntidades}
+                data={entidades}
                 keyExtractor={(item) => item.id.toString()}
                 contentContainerStyle={styles.list}
                 renderItem={({ item }) => (
@@ -27,14 +48,15 @@ export function ListaScreen() {
                             onPress={() => viewModel.deletar(item.id)}>
                             <MaterialIcons name="delete" size={24} color="#E74C3C" />
                         </TouchableOpacity>
-
-
                     </View>
                 )}
             />
             <Text style={styles.header}>Adicionar Nova Entidade</Text>
-            <Button title="Adicionar" color="#C8A2C8" onPress={() => navigation.navigate('Formulario')} />
+            <Button
+                title="Adicionar"
+                color="#C8A2C8"
+                onPress={() => navigation.navigate('Formulario')}
+            />
         </View>
-    )
-
+    );
 }
